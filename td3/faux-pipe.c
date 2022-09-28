bc87e6cbf050facf507411323a155d8d931fc752
Guillaume Dindart <guillaume.dindart@etu.u-bordeaux.fr>
2022-09-28 10:39:20 +0200

    Add td3

---
+++ td3/faux-pipe.c
@@ -0,0 +1,14 @@
+#include <errno.h>
+#include <fcntl.h> 
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SUFFIXE ".idx"
+
+int main(int argc, char *argv[]){
+    return 0;
+}
